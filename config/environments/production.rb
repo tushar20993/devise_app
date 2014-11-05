851cce01324d070071746c27d623a6a5a1545973
Tushar <tushar20993@gmail.com>
2014-11-05 08:57:07 -0800

    Newchange

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,7 @@ DeviseApp::Application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
